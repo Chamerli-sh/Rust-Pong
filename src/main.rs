e43161131ff2f29edac4da34463fb54a1d062ad0
Roombabatata <fares.setbels@gmail.com>
2021-11-04 21:31:34 +0100

    Draw some shapes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@
 
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::graphics::{self, Color};
+use nalgebra;
 use ggez::event::{self, EventHandler};
+
+const RACKET_HEIGHT: f32 = 100.0;
+const RACKET_HEIGHT_HALF: f32 = RACKET_HEIGHT / 2.0;
+const RACKET_WIDTH: f32 = 20.0;
+const RACKET_WIDTH_HALF: f32 = RACKET_WIDTH / 2.0;
+
+
 
 fn main() {
     let (mut ctx, event_loop) = ContextBuilder::new("Pong_01", "Roombabatata")
@@ -16,14 +24,17 @@ fn main() {
 }
 
 struct MainState {
-    // Your state here...
+    player_1_pos: nalgebra::Point2<f32>,    
+    player_2_pos: nalgebra::Point2<f32>,    
 }
 
 impl MainState {
     pub fn new(_ctx: &mut Context) -> MainState {
-        // Load/create resources such as images here.
+        let (screen_widht, screen_height) = graphics::drawable_size(_ctx);
+        let screen_height_half = screen_height / 2.0;
         MainState {
-            // ...
+            player_1_pos : nalgebra::Point2::new(RACKET_WIDTH_HALF, screen_height_half),
+            player_2_pos : nalgebra::Point2::new(screen_widht - RACKET_WIDTH_HALF, screen_height_half),
         }
     }
 }
@@ -34,8 +45,14 @@ impl EventHandler<ggez::GameError> for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, Color::WHITE);
-        // Draw code here...
+        graphics::clear(ctx, Color::BLACK);
+        
+        let racket_rect = graphics::Rect::new(-RACKET_WIDTH_HALF, -RACKET_HEIGHT_HALF, RACKET_WIDTH, RACKET_HEIGHT);
+        let racket_mesh = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), racket_rect, Color::WHITE)?;
+        
+        let draw_param = graphics::DrawParam::default();
+        graphics::draw(ctx, &racket_mesh, graphics::DrawParam::default())?;
+
         graphics::present(ctx)
     }
 }
